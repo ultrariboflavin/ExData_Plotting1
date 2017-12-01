b0c435d3112218c6e5a9e7634043150ec37d0c77
ultrariboflavin <33360352+ultrariboflavin@users.noreply.github.com>
2017-12-01 09:28:39 -0800

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -17,13 +17,30 @@ mydata2$Sub_metering_3 <- as.numeric(as.character(mydata2$Sub_metering_3))
 #open png graphics device
 png(filename="plot4.png",width=480,height = 480)
 
-#plot data
+#setup matrix of plots
+par(mfcol=c(2,2))
+
+#plot1
+plot(mydata2$Global_active_power,xaxt="n",type="l",
+     ylab="Global Active Power (kilowatts)", xlab = "")
+axis(1,at = c(1,1440,2880),labels=c("Thu","Fri","Sat"))
+
+#plot2
 with(mydata2, plot(Sub_metering_1,xaxt="n",type="l",xlab = "", ylab = "Energy sub metering"))
 axis(1,at = c(1,1440,2880),labels=c("Thu","Fri","Sat"))
 with(mydata2, lines(Sub_metering_2, col="red"))
 with(mydata2, lines(Sub_metering_3, col="blue"))
 legend("topright",pch="-",col = c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
+#plot3
+plot(mydata2$Voltage,xaxt="n",type="l",
+     ylab="Voltage", xlab = "datetime")
+axis(1,at = c(1,1440,2880),labels=c("Thu","Fri","Sat"))
+
+#plot4
+plot(mydata2$Global_reactive_power,xaxt="n",type="l",
+     ylab="Global_reactive_power", xlab = "datetime")
+axis(1,at = c(1,1440,2880),labels=c("Thu","Fri","Sat"))
 
 #close graphics device
 dev.off()
